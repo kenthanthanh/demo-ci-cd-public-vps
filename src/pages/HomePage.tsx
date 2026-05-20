666227ed6c3f8b86c48aaa698120f05649b19d2e
h2siee <hieukun36@gmail.com>
2026-05-20 14:18:33 +0700

    Try CI/CD flow

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -7,7 +7,7 @@ function HomePage() {
       {/* Hero Section */}
       <section className="hero">
         <h1 className="hero-title">
-          Full-Stack App
+          Full-Stack App that made by h2siee
           <span className="hero-highlight"> Laravel + React + MongoDB</span>
         </h1>
         <p className="hero-subtitle">
